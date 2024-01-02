d758cb4f37e0894409383b7c7ab4dc60ccab41ac
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-01-02 17:09:31 -0300

    Atualizado anos das metas

--- functions/helpers.js
+++ functions/helpers.js
@@ -2,25 +2,25 @@ const { executeQuerySQL } = require('../connect/headCargo');
 
 const helpers = {
    resultados_ano_anterior: async function() {
-      const result = await executeQuerySQL(`SELECT * FROM vis_Fluxo_Caixa_Novas_Metas WHERE ANO = 2022`);
-
-      return result;
-   },
-
-   resultados_ano_atual: async function() {
       const result = await executeQuerySQL(`SELECT * FROM vis_Fluxo_Caixa_Novas_Metas WHERE ANO = 2023`);
 
       return result;
    },
 
+   resultados_ano_atual: async function() {
+      const result = await executeQuerySQL(`SELECT * FROM vis_Fluxo_Caixa_Novas_Metas WHERE ANO = 2024`);
+
+      return result;
+   },
+
    processos_ano_anterior: async function() {
-      const result = await executeQuerySQL(`SELECT * FROM vis_Metas_Processo_Nova WHERE ANO = 2022`);
+      const result = await executeQuerySQL(`SELECT * FROM vis_Metas_Processo_Nova WHERE ANO = 2023`);
 
       return result;
    },
 
    processos_ano_atual: async function() {
-      const result = await executeQuerySQL(`SELECT * FROM vis_Metas_Processo_Nova WHERE ANO = 2023`);
+      const result = await executeQuerySQL(`SELECT * FROM vis_Metas_Processo_Nova WHERE ANO = 2024`);
 
       return result;
    },
@@ -43,13 +43,13 @@ const helpers = {
    },
 
    teus_tons_ano_anterior: async function() {
-      const result = await executeQuerySQL(`SELECT * FROM vis_Metas_Tons_Teus_Nova WHERE ANO = 2022`);
+      const result = await executeQuerySQL(`SELECT * FROM vis_Metas_Tons_Teus_Nova WHERE ANO = 2023`);
 
       return result;
    },
 
    teus_tons_ano_atual: async function() {
-      const result = await executeQuerySQL(`SELECT * FROM vis_Metas_Tons_Teus_Nova WHERE ANO = 2023`);
+      const result = await executeQuerySQL(`SELECT * FROM vis_Metas_Tons_Teus_Nova WHERE ANO = 2024`);
 
       return result;
    }
