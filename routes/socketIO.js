9652a06cf9a54a2d2cd278ac6a07aca4794c8b6f
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-01-05 18:05:35 -0300

    Inserido consulta em tempo real com as ultimas baixas das faturas

--- routes/socketIO.js
+++ routes/socketIO.js
@@ -4,7 +4,15 @@ const { executeQuerySQL } = require('../connect/headCargo');
 const WebSocket = {
     data: [],
     lastId: 0,
-    Init: function(io){
+    lastIdInvoice: 0,
+    Init: async function(io){
+        const novo_result = await WebSocket.toListen();
+        this.lastId = novo_result.IdLogistica_House;
+
+        // Escuta o pagamento de uma fatura
+        const novo_result_Invoice = await WebSocket.toListenInvoice();
+        this.lastIdInvoice = novo_result_Invoice.IdFatura_Financeira_Baixa;
+
         // Configuração do Socket.IO
         io.on('connection', (socket) => {
             console.log('Usuário conectado');
@@ -16,13 +24,24 @@ const WebSocket = {
 
 
             setInterval(async () => {
-                const id = await WebSocket.toListen();
+                const result = await WebSocket.toListen();
+                const resultInvoice = await WebSocket.toListenInvoice();
+
+                const id = result.IdLogistica_House;
+                const idInvoice = resultInvoice.IdFatura_Financeira_Baixa;
+
                 if(id != WebSocket.lastId){
                     WebSocket.lastId = id;
-                    io.emit('NewProcess', 'Atenção tem um novo processo');
+                    io.emit('NewProcess', result);
                     console.log(WebSocket.lastId)
                 }
-            }, 5000);
+
+                if(idInvoice != WebSocket.lastIdInvoice){
+                    WebSocket.lastIdInvoice = idInvoice;
+                    io.emit('NewInvoice', resultInvoice);
+                    console.log(WebSocket.lastIdInvoice)
+                }
+            }, 2000);
         });
 
 
@@ -30,7 +49,11 @@ const WebSocket = {
     },
     toListen: async function(){
         const result = await executeQuerySQL(`SET LANGUAGE Portuguese SELECT TOP 1 * FROM vis_Metas_Processo_Nova ORDER BY IdLogistica_House DESC`);
-        return result[0].IdLogistica_House
+        return result[0]
+    },
+    toListenInvoice: async function(){
+        const result = await executeQuerySQL(`SET LANGUAGE Portuguese SELECT TOP 1 IdFatura_Financeira_Baixa FROM mov_Fatura_Financeira_Baixa ORDER BY IdFatura_Financeira_Baixa DESC`);
+        return result[0]
     }
 }
 
