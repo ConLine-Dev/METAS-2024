9d90c579e37e76c15d8feba95cd4f7f9386db77a
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-03-26 17:09:54 -0300

    Criado tela de erro e seus arquivos de estilo

--- public/assets/js/verifica_acesso.js
+++ public/assets/js/verifica_acesso.js
@@ -1,18 +1,18 @@
 const dados_login = JSON.parse(localStorage.getItem('metasUser'));
 // Função para verificar se o usuario tem acesso ao modulo em questao
 (async function verifica_acesso(){
-// Obtém a URL atual
-let url = window.location.href;
+   // Obtém a URL atual
+   let url = window.location.href;
 
-// Divide a URL pelo caractere '/'
-let partesDaUrl = url.split('/');
+   // Divide a URL pelo caractere '/'
+   let partesDaUrl = url.split('/');
 
-// Obtém a última parte da URL
-let ultimaParte = partesDaUrl[partesDaUrl.length - 1];
+   // Obtém a última parte da URL
+   let ultimaParte = partesDaUrl[partesDaUrl.length - 1];
 
-const acesso = await Thefetch(`/api/acesso?nivel=${ultimaParte}&email=${dados_login.email}`);
+   const acesso = await Thefetch(`/api/acesso?nivel=${ultimaParte}&email=${dados_login.email}`);
 
-if(acesso.length == 0){
-window.location.href = '/error'
-}
+   if(acesso.length == 0){
+      window.location.href = '/erro-401'
+   }
 })();
