e15c8d9eed815c5463db97252ee44c2c15be1828
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-04-01 09:50:50 -0300

    Criado funcao para listar os vendedores e para carregar os dados do mesmo quando clicado no seu card

--- public/assets/js/admin-meta-comercial.js
+++ public/assets/js/admin-meta-comercial.js
@@ -1,3 +1,4 @@
+const admin_cards_comerciais = await Thefetch('/api/admin-cards-comerciais');
 const meses = [
    {label: 'Janeiro', value: 1},
    {label: 'Fevereiro', value: 2},
@@ -24,8 +25,6 @@ const opcoes_meses = new Choices(
       },
       noResultsText: 'Não há opções disponíveis',
       placeholder: false,
-      placeholderValue: 'teste',
-      itemSelectText: 'Press to select',
    }
 );
 
@@ -39,24 +38,91 @@ new Cleave('#input-meta', {
    delimiter: '.',
 });
 
-// Seleciona o vendedor
-function selecionar_vendedor(e) {
+// Função para inserir os vendedores
+async function vendedores(consulta) {
+   const time_vendedores = document.querySelector('#time-vendedores');
 
-   // Remove a classe vendedor selecionado de todo o arquivo quando outro vendedor for selecionado
-   document.querySelectorAll('.vendedor-selecionado').forEach(selecionado => {
-      selecionado.classList.remove('vendedor-selecionado');
-   });
+   for (let i = 0; i < consulta.length; i++) {
+      const item = consulta[i];
+      
+      const card_html = `
+         <div type="button" class="btn col-xxl-4 col-xl-6 col-lg-6 col-md-6 col-sm-12 modal-vendedor" style="border: none;" data-IdVendedor="${item.IdVendedor}">
+            <div class="card custom-card team-member-card">
 
-   e.classList.add('vendedor-selecionado');
+               <div class="teammember-cover-image my-4">
+                  <span class="avatar avatar-xl avatar-rounded">
+                        <img src="https://cdn.conlinebr.com.br/colaboradores/${item.IdVendedor}" alt="">
+                  </span>
+               </div>
+
+               <div class="card-body p-0">
+                  <div class="d-flex flex-wrap align-item-center mt-sm-0 mt-5 justify-content-between border-bottom border-block-end-dashed p-3" style="margin-top: -25px !important;">
+                        <div class="team-member-details flex-fill">
+                           <p class="mb-0 fw-semibold fs-16 text-truncate">
+                              <a href="javascript:void(0);">${item.Nome}</a>
+                           </p>
+                           <p class="mb-0 fs-12 text-muted text-break">${item.EMail}</p>
+                        </div>
+                  </div>
+
+                  <div class="team-member-stats d-sm-flex justify-content-evenly">
+                        <div class="text-center p-3 my-auto">
+                           <p class="fw-semibold mb-0">Propostas Aprovadas</p><span class="text-muted fs-12">${item.Propostas}</span>
+                        </div>
+                        <div class="text-center p-3 my-auto">
+                           <p class="fw-semibold mb-0">Processos</p><span class="text-muted fs-12">${item.Processos}</span>
+                        </div>
+                        <div class="text-center p-3 my-auto">
+                           <p class="fw-semibold mb-0">Lucro Estimado</p><span class="text-muted fs-12">${item.Lucro_Estimado.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</span>
+                        </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      `
+
+      // Adiciona a string HTML ao array
+      time_vendedores.insertAdjacentHTML('beforeend', card_html);
+   }
+};
+
+async function carrega_indicadores_comercial(IdVendedor) {
+   let meta_financeira_comercial = await Thefetch(`/api/meta-financeira-comercial?IdVendedor=${IdVendedor}`);
+   let proposta_meta_comercial = await Thefetch(`/api/proposta-meta-comercial?IdVendedor=${IdVendedor}`);
+   let processos_meta_comercial = await Thefetch(`/api/processos-meta-comercial?IdVendedor=${IdVendedor}`);
 }
 
+async function eventos_cliques() {
+   // Pega todas as classes
+   const vendedor_clicado = document.querySelectorAll('.imagem-vendedores');
+   // Faz um forEach em todas as tags capturadas
+   vendedor_clicado.forEach(elemento => {
+      elemento.addEventListener('click', function() {
+         // Ao clicar em qualquer outro vendedor, vai remover a classe do que estava selecionado e inserir no clicado
+         vendedor_clicado.forEach(selecionado => {
+            selecionado.classList.remove('vendedor-selecionado')
+         });
+
+         elemento.classList.add('vendedor-selecionado')
+      })
+   });
+
+   document.querySelectorAll('.modal-vendedor').forEach(element => {
+      element.addEventListener('click', async function(e) {
+         const IdVendedor = this.getAttribute('data-IdVendedor');
+         await carrega_indicadores_comercial(IdVendedor);
+         $('#exampleModalXl').modal('show');
+      })
+   });
+
+};
 
 async function mostrar_loading() {
    let img = document.getElementById('loading-img');
 
    // Define o caminho do gif
    img.src = "/assets/images/brand-logos/SLOGAN VERMELHO.gif";
-}
+};
 
 async function remover_loading() {
    let corpoDashboard = document.querySelector('.corpo-dashboard');
@@ -64,12 +130,13 @@ async function remover_loading() {
 
    loading.style.display = 'none';
    corpoDashboard.style.display = 'block';
-}
+};
 
 async function main() {
    await mostrar_loading();
-
+   await vendedores(admin_cards_comerciais);
+   await eventos_cliques();
    await remover_loading();
-}
+};
 
 await main();
